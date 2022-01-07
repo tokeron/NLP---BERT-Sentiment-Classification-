c7f59deb98c70f68e9707f982987bb93fc67fb26
Michael Toker <dontokeron@gmail.com>
2022-01-07 16:49:36 +0200

    First model works. Pretraning in progress

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # imports
 import torch
 from pathlib import Path
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer, \
+    BertForMaskedLM, IntervalStrategy, DataCollatorForLanguageModeling
 from sklearn.metrics import f1_score
 from datasets import load_dataset, load_metric
 import transformers
 
-# paths
-DATA_PATH = Path("clean_data/baby")
-OUT_PATH = Path("output")
+# path to the data
+baby_path = Path("clean_data/baby")
+office_products_path = Path("clean_data/baby")
+OUT_PATH = "output"
 
 
+# Function that alculates f1
 def metric_fn(predictions):
     preds = predictions.predictions.argmax(axis=1)
     labels = predictions.label_ids
@@ -18,34 +21,83 @@ def metric_fn(predictions):
 
 
 if __name__ == '__main__':
+    # Define model and get tokenizer and models from the best library in the world - huggingface
     model_name = 'bert-base-uncased'
-
+    bert_for_pretraining = BertForMaskedLM.from_pretrained('bert-base-uncased')
     model_seq_classification = AutoModelForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     # Defining The Datasets
-
-    data_files = {
-        'train': str(DATA_PATH / 'train.csv'),
-        'test': str(DATA_PATH / 'dev.csv')
+    unlabeled_data_files = {
+        'baby': str(baby_path / 'unlabeled.csv'),
+        'office_products': str(office_products_path / 'unlabeled.csv')
     }
+    data_files = {
+        'train': str(baby_path / 'train.csv'),
+        'test': str(office_products_path / 'dev.csv')
+    }
+
+    # Load data
+    unlabeled_raw_datasets = load_dataset("csv", data_files=unlabeled_data_files)
     raw_datasets = load_dataset("csv", data_files=data_files)
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # Tokenize the data
+    tokenized_unlabeled_datasets = unlabeled_raw_datasets.map(tokenizer, input_columns='review',
+                                                              fn_kwargs={"max_length": 256, "truncation": True,
+                                                                         "padding": "max_length"})
+
     tokenized_datasets = raw_datasets.map(tokenizer, input_columns='review',
-                                          fn_kwargs={"max_length": 128, "truncation": True, "padding": "max_length"})
+                                          fn_kwargs={"max_length": 256, "truncation": True, "padding": "max_length"})
+
+    tokenized_unlabeled_datasets.set_format('torch')
     tokenized_datasets.set_format('torch')
 
+    # Unsupervised training by unlabeled data
+    # define the arguments for the trainer
+    bert_pretraining_args = TrainingArguments(
+        output_dir='pytorch_finetuned_model',  # output directory
+        num_train_epochs=3,  # total # of training epochs
+        per_device_train_batch_size=8,  # batch size per device during training (try 16 if needed)
+        per_device_eval_batch_size=8,  # batch size for evaluation
+        warmup_steps=500,  # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,  # strength of weight decay
+        logging_dir='pytorch_finetuned_log',  # directory for storing logs
+        do_train=True,
+        # evaluation_strategy=IntervalStrategy("steps"),
+        eval_steps=2
+    )
+
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, mlm=True, mlm_probability=0.15
+    )
+
+    bert_baby_pretrainer = Trainer(
+        model=bert_for_pretraining,
+        args=bert_pretraining_args,
+        train_dataset=tokenized_unlabeled_datasets['baby'],
+        eval_dataset=tokenized_unlabeled_datasets['baby']
+    )
+
+    # bert_office_products_pretrainer = Trainer(
+    #     model=bert_for_pretraining,
+    #     args=bert_pretraining_args,
+    #     train_dataset=tokenized_unlabeled_datasets['office_products'],
+    #     eval_dataset=tokenized_unlabeled_datasets['office_products']
+    # )
+
+    # Pretraining The Model
+    bert_baby_pretrainer.train()
+    # bert_office_products_pretrainer.train()
+
+
+    # Prepare labels for the supervized learning
     for split in tokenized_datasets:
         tokenized_datasets[split] = tokenized_datasets[split].add_column('label', raw_datasets[split]['label'])
 
-    args = TrainingArguments(output_dir=OUT_PATH, overwrite_output_dir=True, per_device_train_batch_size=64,
-                             per_device_eval_batch_size=128, metric_for_best_model='dev_f1',
+    args = TrainingArguments(output_dir=OUT_PATH, overwrite_output_dir=True, per_device_train_batch_size=16,
+                             per_device_eval_batch_size=16, metric_for_best_model='dev_f1',
                              greater_is_better=True, do_train=True,
-                             num_train_epochs=5)
-    # args = TrainingArguments(output_dir=OUT_PATH, overwrite_output_dir=True, per_device_train_batch_size=64,
-    #                          per_device_eval_batch_size=128, save_strategy='no', metric_for_best_model='dev_f1',
-    #                          greater_is_better=True, evaluation_strategy='epoch', do_train=True,
-    #                          num_train_epochs=5, report_to='none')
+                             num_train_epochs=8,  evaluation_strategy="epoch")
 
     trainer = Trainer(
         model=model_seq_classification,
@@ -57,7 +109,7 @@ if __name__ == '__main__':
     # Training The Model
     trainer.train()
 
-    trainer.evaluate()
+    print(trainer.evaluate())
 
 
 
